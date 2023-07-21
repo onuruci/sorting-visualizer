a3be96b6fb3fcb525fe74690fde44e3f2ee7f728
onuruci <onur.yuce@metu.edu.tr>
2023-07-21 14:06:22 +0300

    coloring quick sort

--- src/algorithms/quickSort.ts
+++ src/algorithms/quickSort.ts
@@ -1,29 +1,47 @@
 import sleep from "./sleep";
-import { green } from "../utils/colors";
+import { green, purple } from "../utils/colors";
 
 let array: number[];
 
-const implementQuick = async (bars: number[], iStart: number, iEnd: number, setBars: any) => {
+const swapView = (index1: number, index2: number, pivot: number, setSelected: any) => {
+  let item1: SelectedItem = {
+    index: index1,
+    color: green,
+  }
+
+  let item2: SelectedItem = {
+    index: index2,
+    color: green,
+  }
+
+  let pivotItem: SelectedItem = {
+    index: pivot,
+    color: purple,
+  }
+
+  setSelected([item1, item2, pivotItem]);
+
+}
+
+const implementQuick = async (bars: number[], iStart: number, iEnd: number, setBars: any, setSelected: any) => {
   if (iStart >= iEnd || iStart < 0) {
     return;
   }
 
-  let p = await partition(array, iStart, iEnd, setBars);
+  let p = await partition(array, iStart, iEnd, setBars, setSelected);
 
-  implementQuick(array, iStart, p - 1, setBars);
-
-  setBars([...array]);
-  //await sleep(50);
-
-  implementQuick(array, p, iEnd, setBars);
+  await implementQuick(array, iStart, p - 1, setBars, setSelected);
 
   setBars([...array]);
-  //await sleep(150);
+
+  await implementQuick(array, p, iEnd, setBars, setSelected);
+
+  setBars([...array]);
 
   return;
 };
 
-const partition = async (array: number[], iStart: number, iEnd: number, setBars: any) => {
+const partition = async (array: number[], iStart: number, iEnd: number, setBars: any, setSelected: any) => {
   let pivot = array[iEnd];
   let pivotIndex = iStart - 1;
 
@@ -33,6 +51,10 @@ const partition = async (array: number[], iStart: number, iEnd: number, setBars:
       let temp = array[pivotIndex];
       array[pivotIndex] = array[i];
       array[i] = temp;
+
+      swapView(i, pivotIndex, iEnd, setSelected);
+      await sleep(100);
+
       setBars([...array]);
       await sleep(100);
     }
@@ -42,6 +64,9 @@ const partition = async (array: number[], iStart: number, iEnd: number, setBars:
   let temp = array[pivotIndex];
   array[pivotIndex] = array[iEnd];
   array[iEnd] = temp;
+  swapView(iEnd, pivotIndex, iEnd, setSelected);
+  await sleep(100);
+
   setBars([...array]);
   await sleep(100);
 
@@ -51,11 +76,11 @@ const partition = async (array: number[], iStart: number, iEnd: number, setBars:
 const quickSort = async (bars: number[], setBars: any, setSelected: any, pace: number) => {
   array = [...bars];
 
-  await implementQuick(array, 0, array.length - 1, setBars);
+  await implementQuick(array, 0, array.length - 1, setBars, setSelected);
 
-  console.log(array);
-
+  setSelected([]);
   setBars([...array]);
+
 
 };
 
